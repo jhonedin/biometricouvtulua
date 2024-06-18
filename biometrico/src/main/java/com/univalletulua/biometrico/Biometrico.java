577f81e1bb014d313c12eaec2429526ba29c4753
jhonedin <jhonedin1888@hotmail.com>
2024-06-18 11:57:13 -0500

    Se pone a funcionar un primer ejemplo sin conexion a BD

--- biometrico/src/main/java/com/univalletulua/biometrico/Biometrico.java
+++ biometrico/src/main/java/com/univalletulua/biometrico/Biometrico.java
@@ -1,16 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.univalletulua.biometrico;
 
+import com.digitalpersona.onetouch.ui.swing.sample.UISupport.MainForm;
+//import com.digitalpersona.onetouch.ui.swing.sample.Enrollment.MainFormDos;
 /**
  *
- * @author USER
+ * @author JHON EDINSON BLANDON QUINTERO
  */
 public class Biometrico {
-
-    public static void main(String[] args) {
-        System.out.println("*** SISTEMA BIOMETRICO PARA EL CONTROL DE ASISTENCIA DOCENTE UVTULUA ***");
+    public static void main (String[] args) {
+        System.out.println("HOLA JHON EDINSON");
+        MainForm mainF = new MainForm();
+        
+        //MainFormDos mainFDos = new MainFormDos();
+        
     }
 }
+

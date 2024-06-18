2a8ed7844214771d01df6cacc3358f465ade9991
jhonedin <jhonedin1888@hotmail.com>
2024-06-17 20:12:16 -0500

    Se crea el proyecto.

---
+++ biometrico/src/main/java/com/univalletulua/biometrico/Biometrico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.univalletulua.biometrico;
+
+/**
+ *
+ * @author USER
+ */
+public class Biometrico {
+
+    public static void main(String[] args) {
+        System.out.println("*** SISTEMA BIOMETRICO PARA EL CONTROL DE ASISTENCIA DOCENTE UVTULUA ***");
+    }
+}

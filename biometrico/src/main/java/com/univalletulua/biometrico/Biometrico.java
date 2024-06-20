5a56d025ebf8ca145962df1a7e0e7ce22d9c9450
jhonedin <jhonedin1888@hotmail.com>
2024-06-20 16:46:24 -0500

    Se comienzan a crear las ventanas

--- biometrico/src/main/java/com/univalletulua/biometrico/Biometrico.java
+++ biometrico/src/main/java/com/univalletulua/biometrico/Biometrico.java
@@ -1,18 +1,31 @@
 package com.univalletulua.biometrico;
 
-import com.digitalpersona.onetouch.ui.swing.sample.UISupport.MainForm;
-//import com.digitalpersona.onetouch.ui.swing.sample.Enrollment.MainFormDos;
+import Vista.MainFrame;
+
+import javax.swing.*;
+import java.awt.*;
 /**
  *
  * @author JHON EDINSON BLANDON QUINTERO
  */
-public class Biometrico {
+
+
+
+public class Biometrico extends JFrame {
+    
+   
     public static void main (String[] args) {
         System.out.println("HOLA JHON EDINSON");
-        MainForm mainF = new MainForm();
-        
-        //MainFormDos mainFDos = new MainFormDos();
-        
+        System.out.println("AQUI VAMOS DE NUEVO");
+        //MainFrame mainframe = new MainFrame();
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainFrame().setVisible(true);
+            }
+        });
+       
     }
+    
 }
 
